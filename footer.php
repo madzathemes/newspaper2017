f0a788ac7e9c88e1f434223990b9433ba960e441
Madars Bitenieks <madza@madzathemes.com>
2017-04-18 00:21:27 +0300

    fix

--- footer.php
+++ footer.php
@@ -75,6 +75,11 @@
 			<?php } ?>
 		</div>
 	<?php } ?>
+	<div class="mt-f-data"><div  data-facebook="<?php $data = get_option("facebook_username"); if(!empty($data)) { echo esc_attr($data); } else { echo "empty"; } ?>"
+			data-token="<?php $data = get_option("facebook_token"); if(!empty($data)) { echo esc_attr($data); } else { echo "empty"; } ?>"
+			data-twitter="<?php $data = get_option("twitter_username"); if(!empty($data)) { echo esc_attr($data); } else { echo "empty"; } ?>"
+			data-youtube="<?php  $data = get_option("youtube_username"); if(!empty($data)) { echo esc_attr($data); } else { echo "empty"; } ?>"></div></div>
+	</div>
 	</div>
 </div>
 <a href="#" class="footer-scroll-to-top"></a>
